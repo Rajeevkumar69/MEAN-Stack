4ad05b28b9d1c7ad637a89e76cb9cde098b42f20
RajeevKumar69 <rk488296@gmail.com>
2025-07-30 07:59:50 +0530

    Make Simple and Basic Website

--- website.js
+++ website.js
@@ -3,28 +3,39 @@ const fs = require('fs');
 const arg = process.argv;
 
 http.createServer((req, res) => {
-    let collectHeaderData; 
-    fs.readFile('html/header.html', 'utf-8', (err, headerData) => {
-        if (err) {
-            res.writeHead(500, { "content-type": 'text/plain' });
-            res.end('Header not found');
-            return;
-        }
-        collectHeaderData = headerData;
-        // res.writeHead(200, { "content-type": 'text/html' });
-        // res.end(headerData);
-    });
-    if (req.url == '/') {
-        fs.readFile('html/home.html', 'utf-8', (err, data) => {
+
+    // Best is sync approach
+    let collectHeaderData = fs.readFileSync('html/header.html', 'utf-8');
+    let collectedFooterData = fs.readFileSync('html/footer.html', 'utf-8');
+
+    // fs.readFile('html/header.html', 'utf-8', (err, headerData) => {
+    //     if (err) {
+    //         res.writeHead(500, { "content-type": 'text/plain' });
+    //         res.end('Header not found');
+    //         return;
+    //     }
+    //     collectHeaderData = headerData;
+    //     // res.writeHead(200, { "content-type": 'text/html' });
+    //     // res.end(headerData);
+    // });
+
+    let file = '/home';
+    if (req.url !== '/' && req.url !== '/style/style.css') {
+        file = req.url;
+    }
+
+    if (req.url === '/' || (!req.url.endsWith('.css') && !req.url.includes('.'))) {
+        let file = req.url === '/' ? '/home' : req.url;
+
+        fs.readFile("html" + file + ".html", 'utf-8', (err, data) => {
             if (err) {
                 res.writeHead(500, { "content-type": 'text/plain' });
                 res.end('Internal server error');
                 return;
             }
-            
-            
+
             res.writeHead(200, { "content-type": 'text/html' });
-            res.write(collectHeaderData+""+data )
+            res.write(collectHeaderData + "" + data + "" + collectedFooterData)
             res.end();
         });
     } else if (req.url == '/style/style.css') {
@@ -41,5 +52,6 @@ http.createServer((req, res) => {
         res.writeHead(404, { "content-type": 'text/plain' });
         res.end('404 Not Found');
     }
+
 
 }).listen(arg[2]);
