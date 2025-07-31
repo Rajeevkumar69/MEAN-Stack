f6c55dc08d1bd6005f45841dbcca35b184128e9b
RajeevKumar69 <rk488296@gmail.com>
2025-07-31 07:57:22 +0530

    Module Type in package.json Explained

--- express-Js/index.js
+++ express-Js/index.js
@@ -1,12 +1,22 @@
-const express = require('express');
+import express from 'express';
+import home from './pages/home.js';
+import about, { contact } from './pages/about.js';
+
 const app = express();
 
-app.get("", (req, res) => {
-    res.send("<h1>Hello World, ExpressJs</h1>");
-});
-app.get("/dashboard", (req, res) => {
-    res.send("<h1>ExpressJs Dashboard</h1>");
+
+app.get("/", (req, res) => {
+    res.send(home()); 
 });
 
-app.listen(4800);
+app.get("/about", (req, res) => {
+    res.send(about()); 
+});
 
+app.get("/contact", (req, res) => {
+    res.send(contact());
+});
+
+app.listen(4800, () => {
+    console.log("Server is running on http://localhost:4800");
+});
