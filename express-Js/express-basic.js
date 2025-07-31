8e48666bc5b0263e501de65a2b5a1cedb86b958c
RajeevKumar69 <rk488296@gmail.com>
2025-07-31 06:37:31 +0530

    Express Js Basic

---
+++ express-Js/express-basic.js
@@ -0,0 +1,3 @@
+// Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.
+// Express.js is a web application framework for NodeJs. it simplifies building web servers & API's in Nodejs.
+
