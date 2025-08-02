da06b605e2b40fd5a993922d404410ca6ece8d4b
RajeevKumar69 <rk488296@gmail.com>
2025-08-02 15:13:25 +0530

    30 What is Middleware in express js

--- express-Js/index.js
+++ express-Js/index.js
@@ -24,30 +24,60 @@
 
 // Render HTML file from another path
 
-import express from 'express';
+// import express from 'express';
+// import path from 'path';
+// const arg = process.argv;
+
+// const app = express();
+// const viewsPath = path.resolve('views');
+// const stylePath = path.resolve('style');
+
+// app.use(express.static(stylePath));
+
+
+// app.get('/', (req, res) => {
+//   res.sendFile(viewsPath + '/home.html');
+// });
+
+// app.get('/dashboard', (req, res) => {
+//   res.sendFile(viewsPath + '/dashboard.html');
+// });
+
+// // load 404 page
+
+// app.use((req, res) => {
+//   res.sendFile(viewsPath + '/error-page.html');
+// });
+
+
+// app.listen(arg[2]);
+
+// Middleware details with Node & ExpressJs
+
+import express from "express";
 import path from 'path';
 const arg = process.argv;
 
 const app = express();
-const viewsPath = path.resolve('views');
-const stylePath = path.resolve('style');
 
-app.use(express.static(stylePath));
 
+app.use((req, res, next)=>{
+ console.log(req.url);
+    next();
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(viewsPath + '/home.html');
+    res.send('Hello World!');
+});
+
+app.get('/home', (req, res) => {
+    res.send('Home World!');
 });
 
 app.get('/dashboard', (req, res) => {
-  res.sendFile(viewsPath + '/dashboard.html');
+    res.send('Dashboard World!');
 });
 
-// load 404 page
-
-app.use((req, res) => {
-  res.sendFile(viewsPath + '/error-page.html');
-});
 
 
 app.listen(arg[2]);
