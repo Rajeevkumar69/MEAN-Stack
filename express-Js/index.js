6d78d5ccc3a347f21c4033620c2013d612f04b8f
RajeevKumar69 <rk488296@gmail.com>
2025-08-02 13:04:12 +0530

    Render HTML File in ExpressJs

--- express-Js/index.js
+++ express-Js/index.js
@@ -1,22 +1,41 @@
+// import express from 'express';
+// import home from './pages/home.js';
+// import about, { contact } from './pages/about.js';
+
+// const app = express();
+
+
+// app.get("/", (req, res) => {
+//     res.send(home()); 
+// });
+
+// app.get("/about", (req, res) => {
+//     res.send(about()); 
+// });
+
+// app.get("/contact", (req, res) => {
+//     res.send(contact());
+// });
+
+// app.listen(4800, () => {
+//     console.log("Server is running on http://localhost:4800");
+// });
+
+
 import express from 'express';
-import home from './pages/home.js';
-import about, { contact } from './pages/about.js';
+import path from 'path';
+const arg = process.argv;
 
 const app = express();
 
-
-app.get("/", (req, res) => {
-    res.send(home()); 
+app.get('/', (req, res) => {
+  const absPath = path.resolve('views/home.html');
+  res.sendFile(absPath);
 });
 
-app.get("/about", (req, res) => {
-    res.send(about()); 
+app.get('/dashboard', (req, res) => {
+  const absPath = path.resolve('views/dashboard.html');
+  res.sendFile(absPath);
 });
 
-app.get("/contact", (req, res) => {
-    res.send(contact());
-});
-
-app.listen(4800, () => {
-    console.log("Server is running on http://localhost:4800");
-});
+app.listen(arg[2]);
