68890dca2af85cefc3cc05f0edf0f548c509026a
RajeevKumar69 <rk488296@gmail.com>
2025-08-15 08:17:08 +0530

    #57 Fix CORS Issues in Node.js APIs (Cross-Origin Resource Sharing

--- express-Js/index.js
+++ express-Js/index.js
@@ -689,83 +689,112 @@
 
 // dbConection();
 
+/*
+ & CRUD API With Mongoose
+ */
+
 // #54 | GET REST API with Mongoose to Fetch Data from MongoDB
 
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+// import express from "express";
+// import studentModel from "./models/student-model.js";
+
+// const app = express();
+// app.use(express.json());
+
+// await mongoose.connect('mongodb://localhost:27017/school').then(() => {
+//     console.log('___________Connected_______________');
+
+// });
+
+// app.get('/', async (req, res) => {
+//     const studentModelData = await studentModel.find();
+//     res.send(studentModelData);
+// });
+
+// // #55 | Make Post method REST API with Mongoose to insert data
+
+// app.post("/save", async (req, res) => {
+//     try {
+//         const studentModelData = await studentModel.create(req.body);
+//         res.send({
+//             message: 'Data Stored',
+//             success: true,
+//             data: studentModelData
+//         });
+//     } catch (error) {
+//         res.status(500).send({
+//             message: 'Server Error',
+//             success: false,
+//             error: error.message
+//         });
+//     }
+// });
+
+// // #56 Make PUT & DELETE REST API with Mongoose
+
+// app.put("/update/:id", async (req, res) => {
+//     let userId = req.params.id;
+
+//     try {
+//         const studentModelData = await studentModel.findByIdAndUpdate(userId, req.body);
+//         res.send({
+//             message: 'Data Updated',
+//             success: true,
+//             data: studentModelData
+//         });
+//     } catch (error) {
+//         res.status(500).send({
+//             message: 'Server Error',
+//             success: false,
+//             error: error.message
+//         });
+//     }
+// })
+
+// app.delete("/delete/:id", async (req, res) => {
+//     let userId = req.params.id;
+
+//     try {
+//         const studentModelData = await studentModel.findByIdAndDelete(userId);
+//         res.send({
+//             message: 'Data Deleted',
+//             success: true,
+//             data: studentModelData
+//         });
+//     } catch (error) {
+//         res.status(500).send({
+//             message: 'Server Error',
+//             success: false,
+//             error: error.message
+//         });
+//     }
+// })
+
+
+// app.listen(4800);
+
+/*
+ & 57 Fix CORS Issues in Node.js APIs (Cross-Origin Resource Sharing)
+ */
+
 import express from "express";
-import studentModel from "./models/student-model.js";
+import cors from 'cors';
+const arg = process.argv;
+
 
 const app = express();
-app.use(express.json());
+app.use(cors());
 
-await mongoose.connect('mongodb://localhost:27017/school').then(() => {
-    console.log('___________Connected_______________');
-
-});
-
-app.get('/', async (req, res) => {
-    const studentModelData = await studentModel.find();
-    res.send(studentModelData);
-});
-
-// #55 | Make Post method REST API with Mongoose to insert data
-
-app.post("/save", async (req, res) => {
-    try {
-        const studentModelData = await studentModel.create(req.body);
-        res.send({
-            message: 'Data Stored',
-            success: true,
-            data: studentModelData
-        });
-    } catch (error) {
-        res.status(500).send({
-            message: 'Server Error',
-            success: false,
-            error: error.message
-        });
-    }
-});
-
-// #56 Make PUT & DELETE REST API with Mongoose
-
-app.put("/update/:id", async (req, res) => {
-    let userId = req.params.id;
-
-    try {
-        const studentModelData = await studentModel.findByIdAndUpdate(userId, req.body);
-        res.send({
-            message: 'Data Updated',
-            success: true,
-            data: studentModelData
-        });
-    } catch (error) {
-        res.status(500).send({
-            message: 'Server Error',
-            success: false,
-            error: error.message
-        });
-    }
+app.get("/", (req, res) => {
+    res.send({
+        "name": 'Rajeev Kumar',
+        "age": 25,
+        "email": 'rk@test.com'
+    });
 })
 
-app.delete("/delete/:id", async (req, res) => {
-    let userId = req.params.id;
+app.listen(arg[2], () => {
+    console.log(`Server is runing on ${arg[2]}`);
 
-    try {
-        const studentModelData = await studentModel.findByIdAndDelete(userId);
-        res.send({
-            message: 'Data Deleted',
-            success: true,
-            data: studentModelData
-        });
-    } catch (error) {
-        res.status(500).send({
-            message: 'Server Error',
-            success: false,
-            error: error.message
-        });
-    }
 })
-
-
-app.listen(4800);
